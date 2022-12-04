239680929227217d68e9bae55e23864b8f7da0d7
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2022-12-04 17:29:53 +0100

    Product__Sum

---
+++ ProductSum/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProductSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> array = new List<object> { 5,2, new List<object>{ 7, -1 }
+                          ,3, new List<object> { 6, new List<object> { -13,8},4 } };
+
+            Console.WriteLine($"The output is : {ProductSum(array)}");
+            Console.ReadKey();
+        }
+
+        public static int ProductSum(List<object> array)
+        {
+            if (array.Count < 1)
+            {
+                return 0;
+            }
+            return CalculateSum(array,1);
+        }
+
+        public static int CalculateSum(List<object> array, int depth)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (array[i].GetType().IsValueType && array[i] is int)
+                {
+                    sum += int.Parse(array[i].ToString());
+                }
+                else
+                {
+                    sum += CalculateSum(array[i] as List<object>, depth + 1);
+                }
+            }
+
+            return sum * depth;
+        }
+    }
+}
