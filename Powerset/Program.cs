b43e8c9ed52d05ccd554646cbd6ce1e1f167653e
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-02-02 01:09:07 +0100

    Powerset

---
+++ Powerset/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Powerset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new List<int> { 1, 2, 3 };
+
+            var powerSet = Powerset(array);
+
+            foreach (var item in powerSet)
+            {
+                Console.WriteLine($"{String.Join<int>(',',item)}");
+            }
+            
+        }
+
+        public static List<List<int>> Powerset(List<int> array)
+        {
+            var result = new List<List<int>>();
+
+            var set = 
+
+            return result;
+        }
+
+        
+    }
+}
