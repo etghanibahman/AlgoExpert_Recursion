9e48e082e887904ad2b671d8983d0b62e9afce80
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-03-10 22:49:06 +0100

    phone number mnemonics - completed

---
+++ PhoneNumberMnemonics/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneNumberMnemonics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phoneNumber = "1905";
+            Console.WriteLine($"{String.Join(',', PhoneNumberMnemonics(phoneNumber))}");
+        }
+
+        public static List<string> PhoneNumberMnemonics(string phoneNumber)
+        {
+            var mnemonics = new List<string>();
+            PhoneNumberMnemonicsHelper(phoneNumber, 0,new StringBuilder(phoneNumber), mnemonics);
+            return mnemonics;
+        }
+
+        public static void PhoneNumberMnemonicsHelper(string phoneNumber, int idx, StringBuilder mnemonic, List<string> mnemonics)
+        {
+            if (idx == phoneNumber.Length)
+            {
+                mnemonics.Add(mnemonic.ToString());
+                return;
+            }
+            foreach (var c in digitToChar[int.Parse(phoneNumber[idx].ToString())])
+            {
+                mnemonic[idx] = c;
+                PhoneNumberMnemonicsHelper(phoneNumber,idx + 1, mnemonic, mnemonics);
+            }
+        }
+
+        private static Dictionary<int, string> digitToChar = new Dictionary<int, string>() {
+            { 0, "0" },
+            { 1, "1" },
+            { 2, "abc" },
+            { 3, "def" },
+            { 4, "ghi" },
+            { 5, "jkl" },
+            { 6, "mno" },
+            { 7, "pqrs" },
+            { 8, "tuv" },
+            { 9, "wxyz" },
+        };
+    }
+}
